210c10036a4d90b6f09d334148084588df086f16
Clênio Galvão <clenio.cgm@gmail.com>
2021-01-21 17:40:13 -0300

    ex 7 a 10

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import Home from './Home';
 import About from './About';
 import Users from './Users';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/users" component={Users}/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/users/:id" render={(props) => <Users {...props} greetingMessage="Good Morning" />} />
+        </Switch>
         <a href="/">Home</a>
         <a href="/about">About</a>
-        <a href="/users">Users</a>
+        <a href="/users/1">Users</a>
+        
       </BrowserRouter>
     );
   }
